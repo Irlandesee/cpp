9f5e78d2fcce629d3400dd7d664f667e6c5b5d27
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-02-11 19:00:58 +0100

    Solved 1313

--- leetcode/1313/README.md
+++ leetcode/1313/README.md
@@ -1,4 +1,4 @@
-#PROBLEM DESCPRIPTION:
+#PROBLEM DESCRIPTION:
 We are given a list nums of integers representing a list compressed
 with run-length encoding
 
@@ -14,7 +14,7 @@ output : [2,4,4,4]
 
 explanation : The first pair [1,2] means we have freq = 1 and val 2
 				so we generate the array [2]
-The second pair [3,4] means we ahve freq = 3 and val = 4 so we generate [4,4,4]
+The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4]
 At the end the concatenation [2] + [4,4,4] is [2,4,4,4]
 
 #Example 2:
--- leetcode/1313/main.cpp
+++ leetcode/1313/main.cpp
@@ -6,6 +6,46 @@
 
 using namespace std;
 
+vector<int> splitString(const string& str){
+	stringstream ss(str);
+	string token;
+	vector<int> nums;
+	char delim = ',';
+
+	while(getline(ss, token, delim)){
+		int num = stoi(token);
+		nums.push_back(num);
+	}
+
+	return nums;
+}
+
+void printVector(const vector<int>& v){
+	cout << "\n";
+	for(auto var : v)
+		cout << var << " ";
+}
+
+
+vector<int> decompressList(const vector<int>& nums){
+	int freq = 0;
+	int val = 0;
+
+	vector<int> l;
+
+	for(int i = 0; i < nums.size(); i+=2){
+		if(i <= nums.size()-1){
+			freq = nums[i];	
+			val = nums[i+1];
+			
+			for(int j = 0; j < freq; j++)
+				l.push_back(val);
+		}
+
+	}
+	return l;
+}
+
 
 int main(int argc, char** argv){
 	if(argc < 2){
@@ -20,9 +60,22 @@ int main(int argc, char** argv){
 		ifstream f(filename);
 
 		vector<string> lines;
+		vector<int> nums;
+
+		if(f.is_open()){
+			while(getline(f, line)){
+				nums = splitString(line);
+				cout << "\nBefore decompressing: \n";
+				printVector(nums);
+				nums = decompressList(nums);
+				cout << "\nAfter decompressing: \n";
+				printVector(nums);
+			}
+
+		}
 
 		
-
+		return 0;
 	}
 
 }
